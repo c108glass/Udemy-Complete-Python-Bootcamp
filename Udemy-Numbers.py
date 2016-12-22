14725b64308268b598037a8d2bd4b7ac73369ad6
Chris Glass <c108glass@gmail.com>
2016-12-22 14:15:59 -0700

    Add files via upload

---
+++ Udemy-Numbers.py
@@ -0,0 +1,30 @@
+##Numbers
+##-Integers and Floats
+##-Basic Arithmetic
+##-Differences between Pythong 2 vs 3 in division
+##-Object Assignment
+
+##Basic Arithmetic 
+print '2+2 =', 2+2
+print '2-2 =', 2-2
+print '2*2 =', 2*2
+print '2/2 =', 2/2
+print 'Power: 2**2 =', 2**2
+print 'Square Root: 2**0.5 =', 2**0.5
+
+##Dividing in Python 2
+print 'In Python 2, dividing integers are truncated:'
+print '3/2 =', 3/2
+print 'So we can use at least one float:'
+print '3/2.0 =', 3/2.0
+print 'Or we can cast a float like this:'
+print 'float(3)/2 =', float(3)/2
+
+##Assigning labels to objects to create variables
+a = 5
+print 'Variable label:'
+print 'a = 5'
+print 'a+a =', a+a
+a = a+a
+print 'a = a+a'
+print 'a+a =', a+a
